b419e858361f5d48c3879658bfa45b4bc346a29f
ahmetemrecamm <56602436+ahmetemrecamm@users.noreply.github.com>
2020-01-02 13:01:09 +0300

    Update AhmetEmreCam2013280043_Odev3.cpp

--- AhmetEmreCam2013280043_Odev3.cpp
+++ AhmetEmreCam2013280043_Odev3.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 using namespace std;
 /* Ahmet Emre Cam 2013280043 */
-void siralama(int dizi[],int sayi) /* bubble sort siralamasý ile notlarý sýralama */
+void siralama(int dizi[],int sayi) /* bubble sort siralamasi ile notlari siralama */
 	{	
 	int temp;
 		for(int i = 0; i < sayi; i++)
@@ -37,7 +37,7 @@ int medyanBulma(int dizi[],int sayi)
 	return Medyan;
 	}
 	else
-	Medyan=(dizi[medyanTemp]+dizi[medyanTemp+1])/2; /* eger ogrenci sayýsý tekse ortadaki sayýlarý toplayýp 2 ye bölere medyaný bulma */
+	Medyan=(dizi[medyanTemp]+dizi[medyanTemp+1])/2; /* eger ogrenci sayisi tekse ortadaki sayilarin ortalamasini alma */
 	return Medyan;
 	
 	}
@@ -57,7 +57,7 @@ int main()
 		Dosya >> cevapAnahtari[i];
 	}	
 	Dosya >> ogrenciSayisi;
-	string ogrenciler [ogrenciSayisi][toplamSoru+1] = {}; /* input dosyasýndaki bilgileri diziye aktarma */
+	string ogrenciler [ogrenciSayisi][toplamSoru+1] = {}; /* input dosyasÃ½ndaki bilgileri diziye aktarma */
 	for (int i=0; i<ogrenciSayisi; i++)
 	{
 		for (int j=0; j<(toplamSoru+1); j++)
@@ -66,7 +66,7 @@ int main()
 		}
 	}
 	int Sonuc[ogrenciSayisi] = {}; 
-	for (int i=0; i<ogrenciSayisi; i++) /* cevaplara göre puanlandýrma */
+	for (int i=0; i<ogrenciSayisi; i++) /* cevaplara gÃ¶re puanlandÃ½rma */
 	{
 		notlar = 0; 
 		for (int j=0; j<toplamSoru; j++)
@@ -74,7 +74,7 @@ int main()
 			if (ogrenciler[i][j+1] == cevapAnahtari[j]) 
 			notlar = notlar + 4; 		
 				
-			else if (ogrenciler[i][j+1] == "f") /* boþ soruyu f olarak tanýmladým */
+			else if (ogrenciler[i][j+1] == "f") /* boÃ¾ soruyu f olarak tanÃ½mladÃ½m */
 			notlar = notlar + 0; 	
 					
 			else
@@ -88,23 +88,23 @@ int main()
 	maxNot = (toplamSoru*4);
 	for (int i=0; i<ogrenciSayisi; i++)
 	{
-		Sonuc[i] = (Sonuc[i] * 100) / maxNot; /* soru sayýsýna gore puaný 100 luk sisteme cevirme */
+		Sonuc[i] = (Sonuc[i] * 100) / maxNot; /* puanÄ± 100 luk sisteme cevirme */
 	}	
 	siralama(Sonuc,ogrenciSayisi);
-	max=Sonuc[ogrenciSayisi-1]; /* dizi artýk sýralý olduðu için dizinin son elemaný max ilk elamaný min ( dizi 0 dan basladigindan toplam elaman sayisindan -1 cýkarýrsan son elamaný buluruz */
+	max=Sonuc[ogrenciSayisi-1]; /* dizi artik sirali oldugu iÃ§in dizinin son elemani max ilk elamani min oluyor( dizi 0 dan basladigindan toplam elaman sayisindan -1 cikarirsan son elamani buluruz */
 	min=Sonuc[0];
 	aciklik=max-min;
 	Dosya.close();
-	for(int i = (ogrenciSayisi-1); i>=0; i--) /* odevde istenildiði gibi max tan mine sýralama */
+	for(int i = (ogrenciSayisi-1); i>=0; i--) /* odevde istenildigi gibi max tan mine siralama */
 	{
-		Dosya2 << ogrenciler[i][0] << ","; /* dizidikeri dosyaya yazdirma */
+		Dosya2 << ogrenciler[i][0] << ","; /* dizidekileri dosyaya yazdirma */
 		Dosya2 << Sonuc[i] << endl;
 	}
-	Dosya2 << "En yuksek not= "<< max << " En dusuk not= " << min << " Sýnýf ortalamasý= " << ortBulma(Sonuc,ogrenciSayisi) << " Sýnýf medyaný= " << medyanBulma(Sonuc,ogrenciSayisi) << " Sýnýfýn acýklýgý= " << aciklik;	
+	Dosya2 << "En yuksek not= "<< max << " En dusuk not= " << min << " Sinif ortalamasi= " << ortBulma(Sonuc,ogrenciSayisi) << " Sinif medyani= " << medyanBulma(Sonuc,ogrenciSayisi) << " Sinifin Acikligi= " << aciklik;	
 	cout <<endl << "Tamamlandi." << endl;	
 	Dosya2.close();	
 	return 0;
-	/* c++ ý iyi bilmedigim icin c gibi yazýp sonra c++ a cevirdim o yüzden kod karmasýkligi olabilir */
+	/* c++ i iyi bilmedigim icin c gibi yazÃ½p sonra c++ a cevirdim o yÃ¼zden kod karmasikligi olabilir */
 	/*KAYNAKCA http://anaturb.net/C/string_exapm.htm
 				http://www.cplusplus.com/doc/tutorial/files/*/
 }
